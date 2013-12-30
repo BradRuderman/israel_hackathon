3da79ca66e1826283399a87fc5599a46a76ecabb
Brad Ruderman <bruderman@radiumone.com>
2013-12-30 08:47:05 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackathon::Application.config.secret_key_base = 'd5c296e954028ae0ec19170a3d69f3339c27040682f36d9504101a30d8223a8c2a979644f3703c84b4c433ff64a395d3d5c2ad06cafc8603241e6cb6fccdc323'
