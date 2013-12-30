e6c5fb1919b9ddb8f54e9d67c15f4aca4151dd19
Josh Grinberg <jgrinber@stanford.edu>
2013-12-30 14:45:56 +0200

    fixing migration format

--- db/migrate/20131230092415_add_ticket_fields.rb
+++ db/migrate/20131230092415_add_ticket_fields.rb
@@ -1,10 +1,10 @@
 class AddTicketFields < ActiveRecord::Migration
  def self.up
     add_column :tickets, :description, :string
- 	add_column :tickets, :status, :string
-  	add_column :tickets, :priority, :string
-	add_column :tickets, :category, :string
-	add_column :tickets, :private, :string
+    add_column :tickets, :status, :string
+    add_column :tickets, :priority, :string
+    add_column :tickets, :category, :string
+    add_column :tickets, :private, :string
     add_column :tickets, :lat, :decimal, :precision => 10, :scale => 7
     add_column :tickets, :long, :decimal, :precision => 10, :scale => 7
     add_column :tickets, :image, :binary
