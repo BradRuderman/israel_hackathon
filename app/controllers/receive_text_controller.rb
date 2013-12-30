564436866a100d80761420adb03497e1aba336a7
Josh Grinberg <jgrinber@stanford.edu>
2013-12-30 22:15:58 +0200

    creating tickets with twilio sms

---
+++ app/controllers/receive_text_controller.rb
@@ -0,0 +1,16 @@
+class ReceiveTextController < ApplicationController
+  def index
+    # let's pretend that we've mapped this action to
+    # http://localhost:3000/sms in the routes.rb file
+
+    message_body = params["Body"]
+    from_number = params["From"]
+    @ticket = Ticket.create(description: message_body)
+
+    redirect_to controller: 'tickets', action: 'show', id: @ticket.id
+
+    #render json: {message_body: message_body, from_number: from_number}.to_json()
+    #format.all { render :nothing => true, :status => 200, :content_type => 'text/html'}
+
+  end
+end
