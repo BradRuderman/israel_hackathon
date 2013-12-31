07f3b4da162cb8476085f4f1451b4460946d41e0
Josh Grinberg <jgrinber@stanford.edu>
2013-12-31 10:41:10 +0200

    parsing texts for address field

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -1,11 +1,17 @@
 class ReceiveTextController < ApplicationController
   def index
-    # let's pretend that we've mapped this action to
-    # http://localhost:3000/sms in the routes.rb file
-
     message_body = params["Body"]
     from_number = params["From"]
-    @ticket = Ticket.create(description: message_body)
+
+    # TODO verify parsing works correctly
+    address = nil
+    address_line = message_body[/address:.*/]
+    if !address_line.nil?
+      address = address_line.split(/address:\s*/).second
+      message_body = message_body.split(/address:.*/).second
+    end
+
+    @ticket = Ticket.create(description: message_body, address: address)
 
     redirect_to controller: 'tickets', action: 'show', id: @ticket.id
 
